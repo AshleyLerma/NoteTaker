62356a5c947dfbe2451cfe730451dc8c10297c13
Ashley Lerma <lerma.scout@gmail.com>
2020-07-07 15:36:28 -0500

    code added for each route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,17 +6,25 @@ var fs = require("fs");
 module.exports = function (app) {
   // API GET Request
   app.get("/api/notes", function (req, res) {
-    // Should read the db.json file and return all saved notes as JSON.
+    // Read the db.json file and return all saved notes as JSON
     res.json(db);
   });
 
   // API POST Request
   app.post("/api/notes", function (req, res) {
-    // Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    // Receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    db.push(req.body);
+
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
   });
 
   // API DELETE Request
   app.delete("/api/notes/:id", function (req, res) {
-    // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+    // Receive a query parameter containing the id of a note to delete and remove
+    console.log("delete", req.body);
+    db.pop(req.body);
+    res.json(db);
   });
 };
